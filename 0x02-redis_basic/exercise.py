047ee55ff7a752d7cf1cf194359ee6ff094d2e07
Stephen Oba <obastepheno@gmail.com>
2023-04-19 23:54:23 +0100

    ➕ Add file exercise.py
    Create cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,4 +2,35 @@
 """
 Module contains the Cache class
 """
-from typing import List
+import uuid
+import redis
+from typing import Union
+
+
+class Cache:
+    """
+    Mapping for cache object. manages data using Redis
+    """
+    def __init__(self):
+        """
+        Initialize a Cache object
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[int, str, bytes, float]) -> str:
+        """
+        generate a random key, and store the input data in
+        Redis using the random key
+
+        Args
+        ----
+        data (Any): Data to be stoed in the Cache
+
+        Returns
+        -------
+        A key(str) for the data stored
+        """
+        key = uuid.uuid4()
+        self._redis.set({key: data})
+        return key
