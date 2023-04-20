b9c6d234815d7a56d1695d63319b0172a7ee5191
Stephen Oba <obastepheno@gmail.com>
2023-04-20 01:03:48 +0100

    📝 Update function get_page

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,27 +6,19 @@ import redis
 import requests
 from typing import Callable
 
-redis_ = redis.Redis()
+_redis = redis.Redis()
 
 
-def track_requests(method: Callable) -> Callable:
-    """Decorator to track requests made to a url"""
-    @wraps(method)
-    def wrapper(url):
-        """ Wrapper for decorator """
-        redis_.incr(f"count:{url}")
-        cached = redis_.get(f"cached:{url}")
-        if cached:
-            return cached.decode('utf-8')
-        html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
-        return html
-
-    return wrapper
-
-
-@track_requests
 def get_page(url: str) -> str:
-    """Obtain the HTML content of a  URL"""
+    """Obtain the HTML content of a URL"""
+    if _redis.get(f'count:{url}'):
+        _redis.incr(f"count:{url}")
+    else:
+        _redis.set(f'count:{url}', 1)
+    cached = _redis.get(f"cached:{url}")
+    if cached:
+        return cached.decode('utf-8')
     req = requests.get(url)
-    return req.text
+    html = req.text
+    _redis.setex(f"cached:{url}", 10, html)
+    return html
